07c36a0284ca2479229e673a36d5de1c52ef62e2
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 11:34:12 +0200

    Convertit la recette des planches de chêne noir en patterns multiples

--- src/crafts/dark-oak-planks.ts
+++ src/crafts/dark-oak-planks.ts
@@ -3,6 +3,6 @@ import type { CraftingRecipe } from "./types";
 
 export const darkOakPlanksCraft: CraftingRecipe = {
   id: "dark-oak-planks",
-  pattern: [BlockId.DarkOakLog, null, null, null, null, null, null, null, null],
+  patterns: [[BlockId.DarkOakLog, null, null, null, null, null, null, null, null]],
   result: { blockId: BlockId.DarkOakPlanks, count: 4 },
 };
