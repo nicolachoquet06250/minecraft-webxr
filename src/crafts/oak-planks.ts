770a09c2f054096ef706a6695c8899be783af7e6
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 11:30:48 +0200

    Convertit la recette des planches de chêne en patterns multiples

--- src/crafts/oak-planks.ts
+++ src/crafts/oak-planks.ts
@@ -3,6 +3,6 @@ import type { CraftingRecipe } from "./types";
 
 export const oakPlanksCraft: CraftingRecipe = {
   id: "oak-planks",
-  pattern: [BlockId.OakLog, null, null, null, null, null, null, null, null],
+  patterns: [[BlockId.OakLog, null, null, null, null, null, null, null, null]],
   result: { blockId: BlockId.OakPlanks, count: 4 },
 };
