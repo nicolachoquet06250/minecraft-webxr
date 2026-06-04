dedee57fe0f49c68e483cf00a1788173c041722d
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 11:46:47 +0200

    Ajoute les définitions des minerais

---
+++ src/blocks/ore-blocks.ts
@@ -0,0 +1,21 @@
+import { BlockId } from "../types";
+import type { BlockDefinition } from "./types";
+
+export const oreBlockDefinitions: BlockDefinition[] = [
+  { id: BlockId.CoalOre, name: "Coal Ore", color: [0.12, 0.12, 0.12, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.IronOre, name: "Iron Ore", color: [0.78, 0.58, 0.38, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.CopperOre, name: "Copper Ore", color: [0.75, 0.42, 0.25, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.GoldOre, name: "Gold Ore", color: [1.0, 0.78, 0.12, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.RedstoneOre, name: "Redstone Ore", color: [0.8, 0.05, 0.05, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.LapisOre, name: "Lapis Ore", color: [0.08, 0.18, 0.75, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.DiamondOre, name: "Diamond Ore", color: [0.25, 0.9, 0.95, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.EmeraldOre, name: "Emerald Ore", color: [0.1, 0.8, 0.25, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.DeepslateCoalOre, name: "Deepslate Coal Ore", color: [0.12, 0.12, 0.12, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.DeepslateIronOre, name: "Deepslate Iron Ore", color: [0.78, 0.58, 0.38, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.DeepslateCopperOre, name: "Deepslate Copper Ore", color: [0.75, 0.42, 0.25, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.DeepslateGoldOre, name: "Deepslate Gold Ore", color: [1.0, 0.78, 0.12, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.DeepslateRedstoneOre, name: "Deepslate Redstone Ore", color: [0.8, 0.05, 0.05, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.DeepslateLapisOre, name: "Deepslate Lapis Ore", color: [0.08, 0.18, 0.75, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.DeepslateDiamondOre, name: "Deepslate Diamond Ore", color: [0.25, 0.9, 0.95, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.DeepslateEmeraldOre, name: "Deepslate Emerald Ore", color: [0.1, 0.8, 0.25, 1.0], solid: true, transparentForMeshing: false },
+];
