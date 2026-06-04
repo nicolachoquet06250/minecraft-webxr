dfd5be0ebf9dd82758018c94f4bfb784d70e871d
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 15:29:39 +0200

    Ajoute la texture procédurale du sable

--- src/blocks/terrain-blocks.ts
+++ src/blocks/terrain-blocks.ts
@@ -125,6 +125,34 @@ const stoneTexture: BlockTextureDefinition = {
   ],
 };
 
+const sandTexture: BlockTextureDefinition = {
+  palette: {
+    A: [0.98, 0.96, 0.80, 1],
+    B: [0.90, 0.87, 0.68, 1],
+    C: [0.86, 0.83, 0.63, 1],
+    D: [0.83, 0.80, 0.59, 1],
+    E: [0.76, 0.73, 0.52, 1],
+  },
+  matrix: [
+    "ECCCBCDCBCCCCADC",
+    "CCBBEDDDBBACCCCB",
+    "BBADDDCCCBBBCDDA",
+    "DDCCDCDCEDCBBBCE",
+    "BDBDBBBBDDDBDEBE",
+    "DCDADBCCDBCEDDCE",
+    "ECDCECCDDCBACBCD",
+    "CCBCCDBACDCDCBAB",
+    "CCBBCDBCDAECDCCC",
+    "ECBCCCDDCCCBCBCB",
+    "CCEBEBCCBBBDCADC",
+    "DBBECDCDCCCADDDE",
+    "BCDBCDCEECCDCBDD",
+    "CBDEDDDBCABCBDBD",
+    "BCDDCDCCECEDBDDC",
+    "DBCEACEBCBDCCCDE",
+  ],
+};
+
 export const terrainBlockDefinitions: BlockDefinition[] = [
   { id: BlockId.Air, name: "Air", color: [1.0, 1.0, 1.0, 0.0], solid: false, transparentForMeshing: true },
   {
@@ -148,26 +176,12 @@ export const terrainBlockDefinitions: BlockDefinition[] = [
     color: [0.45, 0.28, 0.12, 1.0],
     solid: true,
     transparentForMeshing: false,
-    textures: {
-      top: dirtTexture,
-      bottom: dirtTexture,
-      front: dirtTexture,
-      back: dirtTexture,
-      right: dirtTexture,
-      left: dirtTexture,
-    },
+    textures: allFaces(dirtTexture),
   },
   { id: BlockId.CoarseDirt, name: "Coarse Dirt", color: [0.45, 0.28, 0.12, 1.0], solid: true, transparentForMeshing: false },
   { id: BlockId.Podzol, name: "Podzol", color: [0.36, 0.23, 0.11, 1.0], solid: true, transparentForMeshing: false },
   { id: BlockId.RootedDirt, name: "Rooted Dirt", color: [0.45, 0.28, 0.12, 1.0], solid: true, transparentForMeshing: false },
-  { 
-    id: BlockId.Stone, 
-    name: "Stone", 
-    color: [0.45, 0.45, 0.45, 1.0], 
-    solid: true, 
-    transparentForMeshing: false,
-    textures: allFaces(stoneTexture),
-  },
+  { id: BlockId.Stone, name: "Stone", color: [0.45, 0.45, 0.45, 1.0], solid: true, transparentForMeshing: false, textures: allFaces(stoneTexture) },
   { id: BlockId.Deepslate, name: "Deepslate", color: [0.18, 0.18, 0.2, 1.0], solid: true, transparentForMeshing: false },
   { id: BlockId.Granite, name: "Granite", color: [0.58, 0.36, 0.29, 1.0], solid: true, transparentForMeshing: false },
   { id: BlockId.Diorite, name: "Diorite", color: [0.78, 0.78, 0.76, 1.0], solid: true, transparentForMeshing: false },
@@ -175,7 +189,7 @@ export const terrainBlockDefinitions: BlockDefinition[] = [
   { id: BlockId.Tuff, name: "Tuff", color: [0.32, 0.34, 0.32, 1.0], solid: true, transparentForMeshing: false },
   { id: BlockId.Calcite, name: "Calcite", color: [0.86, 0.84, 0.78, 1.0], solid: true, transparentForMeshing: false },
   { id: BlockId.Gravel, name: "Gravel", color: [0.42, 0.4, 0.38, 1.0], solid: true, transparentForMeshing: false },
-  { id: BlockId.Sand, name: "Sand", color: [0.82, 0.72, 0.42, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.Sand, name: "Sand", color: [0.82, 0.72, 0.42, 1.0], solid: true, transparentForMeshing: false, textures: allFaces(sandTexture) },
   { id: BlockId.RedSand, name: "Red Sand", color: [0.75, 0.33, 0.16, 1.0], solid: true, transparentForMeshing: false },
   { id: BlockId.Clay, name: "Clay", color: [0.48, 0.52, 0.58, 1.0], solid: true, transparentForMeshing: false },
 ];
