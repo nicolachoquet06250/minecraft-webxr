ee615d3cb59e52d87681bd85e4a692962b25a3a2
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 11:22:51 +0200

    Ajoute la recette des planches de sapin

---
+++ src/crafts/spruce-planks.ts
@@ -0,0 +1,8 @@
+import { BlockId } from "../types";
+import type { CraftingRecipe } from "./types";
+
+export const sprucePlanksCraft: CraftingRecipe = {
+  id: "spruce-planks",
+  pattern: [BlockId.SpruceLog, null, null, null, null, null, null, null, null],
+  result: { blockId: BlockId.SprucePlanks, count: 4 },
+};
