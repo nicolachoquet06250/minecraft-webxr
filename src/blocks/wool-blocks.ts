1ed6bd52ff935e1edefb622803a5903ed1cc4035
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 11:51:13 +0200

    Ajoute les définitions des blocs de laine

---
+++ src/blocks/wool-blocks.ts
@@ -0,0 +1,21 @@
+import { BlockId } from "../types";
+import type { BlockDefinition } from "./types";
+
+export const woolBlockDefinitions: BlockDefinition[] = [
+  { id: BlockId.WhiteWool, name: "White Wool", color: [0.95, 0.95, 0.95, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.OrangeWool, name: "Orange Wool", color: [0.95, 0.45, 0.12, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.MagentaWool, name: "Magenta Wool", color: [0.75, 0.2, 0.75, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.LightBlueWool, name: "Light Blue Wool", color: [0.4, 0.65, 0.95, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.YellowWool, name: "Yellow Wool", color: [0.95, 0.85, 0.15, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.LimeWool, name: "Lime Wool", color: [0.45, 0.85, 0.2, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.PinkWool, name: "Pink Wool", color: [0.95, 0.45, 0.7, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.GrayWool, name: "Gray Wool", color: [0.3, 0.3, 0.3, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.LightGrayWool, name: "Light Gray Wool", color: [0.65, 0.65, 0.65, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.CyanWool, name: "Cyan Wool", color: [0.15, 0.55, 0.65, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.PurpleWool, name: "Purple Wool", color: [0.45, 0.2, 0.65, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.BlueWool, name: "Blue Wool", color: [0.15, 0.25, 0.75, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.BrownWool, name: "Brown Wool", color: [0.35, 0.18, 0.08, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.GreenWool, name: "Green Wool", color: [0.18, 0.45, 0.12, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.RedWool, name: "Red Wool", color: [0.75, 0.08, 0.08, 1.0], solid: true, transparentForMeshing: false },
+  { id: BlockId.BlackWool, name: "Black Wool", color: [0.02, 0.02, 0.02, 1.0], solid: true, transparentForMeshing: false },
+];
