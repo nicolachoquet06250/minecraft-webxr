c761c16e8ead09779d71d04a28911eef2fb16a7c
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 11:35:32 +0200

    Convertit la recette des planches de cerisier en patterns multiples

--- src/crafts/cherry-planks.ts
+++ src/crafts/cherry-planks.ts
@@ -3,6 +3,6 @@ import type { CraftingRecipe } from "./types";
 
 export const cherryPlanksCraft: CraftingRecipe = {
   id: "cherry-planks",
-  pattern: [BlockId.CherryLog, null, null, null, null, null, null, null, null],
+  patterns: [[BlockId.CherryLog, null, null, null, null, null, null, null, null]],
   result: { blockId: BlockId.CherryPlanks, count: 4 },
 };
