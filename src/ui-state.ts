09bcb546f695d1ceccc6dac5ac9026a71de72bd8
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 10:31:59 +0200

    Add shared UI state

---
+++ src/ui-state.ts
@@ -0,0 +1,9 @@
+let craftingOverlayOpen = false;
+
+export function setCraftingOverlayOpen(isOpen: boolean): void {
+  craftingOverlayOpen = isOpen;
+}
+
+export function isCraftingOverlayOpen(): boolean {
+  return craftingOverlayOpen;
+}
