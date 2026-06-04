b09d3a018e1e8d032f86f35f4567eb38f44e4f41
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 11:33:34 +0200

    Convertit la recette des planches d'acacia en patterns multiples

--- src/crafts/acacia-planks.ts
+++ src/crafts/acacia-planks.ts
@@ -3,6 +3,6 @@ import type { CraftingRecipe } from "./types";
 
 export const acaciaPlanksCraft: CraftingRecipe = {
   id: "acacia-planks",
-  pattern: [BlockId.AcaciaLog, null, null, null, null, null, null, null, null],
+  patterns: [[BlockId.AcaciaLog, null, null, null, null, null, null, null, null]],
   result: { blockId: BlockId.AcaciaPlanks, count: 4 },
 };
