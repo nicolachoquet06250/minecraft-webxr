4a417b1eac2fa56dff0a6e528397c682e47fe94b
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-04 11:24:59 +0200

    Ajoute la recette des planches de palétuvier

---
+++ src/crafts/mangrove-planks.ts
@@ -0,0 +1,8 @@
+import { BlockId } from "../types";
+import type { CraftingRecipe } from "./types";
+
+export const mangrovePlanksCraft: CraftingRecipe = {
+  id: "mangrove-planks",
+  pattern: [BlockId.MangroveLog, null, null, null, null, null, null, null, null],
+  result: { blockId: BlockId.MangrovePlanks, count: 4 },
+};
