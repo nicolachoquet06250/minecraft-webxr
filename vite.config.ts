db263284f34a27fa7c92afe628734d04c393efa0
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-05 10:00:41 +0000

    amélioration du manifest

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,10 @@ export default defineConfig({
       injectRegister: "auto",
       includeAssets: ["favicon.svg", "items/**/*.png"],
       manifest: {
+        id: "minecraft-webxr",
         name: "Minecraft WebXR",
         short_name: "Minecraft XR",
-        description: "Minecraft-like TypeScript Babylon.js WebXR game",
+        description: "Un clone de Minecraft concu pour être nativement cross-platforms.",
         start_url: "/",
         scope: "/",
         display: "fullscreen",
@@ -22,17 +23,34 @@ export default defineConfig({
         icons: [
           {
             src: "/favicon-192x192.png",
-            sizes: "any",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "/favicon-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable",
           },
+          {
+            src: "/favicon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
         ],
+        screenshots: [
+          {
+            src: "/screenshots/screenshot-desktop.png",
+            sizes: "2879x1799",
+            type: "image/png",
+            "label": "Écran sur desktop"
+          },
+          {
+            src: "/screenshots/screenshot-mobile.png",
+            sizes: "1024x500",
+            type: "image/png",
+            "label": "Écran sur mobile"
+          },
+        ],
+        categories: ["games", "entertainment", "building"],
+        prefer_related_applications: false,
+        dir: "ltr"
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,wasm,svg,png,webp,ico}"],
